2b98d5f147476ab8394c3984a7a8af7e44bf59d6
richasinhaa <richa.sinha0603@gmail.com>
2021-12-14 22:59:41 +0800

    Template for Todoist Clone Api added

---
+++ src/main/java/com/api/todoistClone/TodoistCloneApplication.java
@@ -0,0 +1,13 @@
+package com.api.todoistClone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoistCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoistCloneApplication.class, args);
+	}
+
+}
